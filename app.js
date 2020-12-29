d3daa41f8ffb9e97fcaca36b8de8291abeddd811
lolikaa <monik.gabrys@wp.pl>
2020-12-30 00:16:11 +0100

    node started

---
+++ app.js
@@ -0,0 +1,14 @@
+//require node modules
+const http = require('http');
+
+//file imports
+const respond = require('./lib/respond.js');
+
+// create server
+const port = process.env.port || 8000;
+const server = http.createServer(respond);
+
+//listen port
+server.listen(port, () => {
+    console.log(`nasluchuje portu: ${port}`);
+});
